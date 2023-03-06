344b6fc1e7b5971c47ef262e16510832e702105d
Georg Hirsch <g.hirsch@imrecycling.de>
2023-03-06 20:04:17 +0100

    changed nav background styling. light and dark compatible

--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -6,7 +6,7 @@ import { nav_item_interface } from './nav_item_interface';
 export default function Nav({ items }: { items: nav_item_interface[] }) {
     return (
         <nav className="
-              dark:bg-blue-900 bg-blue-400 bottom-2 border-b-4 border-green-500 py-3 h-36 flex flex-row
+              dark:bg-blue-900 bg-blue-400 bg-gradient-to-br from-sky-400 via-blue-400 dark:from-indigo-900 dark:via-blue-900 bottom-2 border-b-4 border-green-500 py-3 h-36 flex flex-row
         ">
             <ul className="
                 h-4/5 container
