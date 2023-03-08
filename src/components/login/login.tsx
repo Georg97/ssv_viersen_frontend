c61a8a7061be826601bd378c0f11b7c1c169f0d5
Georg Hirsch <g.hirsch@imrecycling.de>
2023-03-08 23:46:01 +0100

    added login component, altough styling is not final

---
+++ src/components/login/login.tsx
@@ -0,0 +1,51 @@
+import Image from 'next/image';
+import {FormEvent, FormEventHandler, useState } from 'react';
+
+function LoginForm() {
+    const [selected, setSelected] = useState(false)
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        console.log("need to implement login functionality")
+        console.log(`Logged in with ${username} - ${password}`)
+    }
+    const usernameChanged = (event: FormEvent<HTMLInputElement>) => {
+        setUsername(event.currentTarget.value)
+    }
+    const passwordChanged = (event: FormEvent<HTMLInputElement>) => {
+        setPassword(event.currentTarget.value)
+    }
+
+    return (
+        <div className='sm:relative'>
+            <Image src='/login_round.png' width='240' height='240' alt='Login-Formular' className='h-full w-auto p-4 hover:cursor-pointer' onClick={() => setSelected(!selected)} />
+            <form
+                onSubmit={handleSubmit}
+                className={`
+                    ${selected ? 'flex' : 'hidden'} flex-col
+                    sm:absolute sm:top-[135%] sm:right-8
+                    soft-gradient
+                    dark:hard-gradient
+                    border-green-500 border-2 rounded-lg relative sm:p-3
+                `}>
+                <button
+                    onClick={() => setSelected(!selected)}
+                    className="absolute top-0 right-0 translate-x-3 -translate-y-3 rounded-full border-2 w-7 h-7 font-bold text-white bg-fuchsia-600 hover:scale-110 transition-all"
+                >X</button>
+                <label className="w-full">
+                    <span className="text-xs">Name</span>
+                    <input type="text" placeholder='Username' onChange={usernameChanged} className="bg-transparent border-b-2 dark:border-indigo-50 border-indigo-900 text-indigo-900 dark:text-indigo-50" />
+                </label>
+                <label className="w-full">
+                    <span className="text-xs">Passwort</span>
+                    <input type="password" placeholder='Passwort' onChange={passwordChanged} className="bg-transparent border-b-2 dark:border-indigo-50 border-indigo-900 text-indigo-900 dark:text-indigo-50" />
+                </label>
+                <input type="submit" value="Login" className='btn mx-auto mt-4' />
+            </form>
+        </div>
+     );
+}
+
+export default LoginForm;
--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import LoginForm from '../login/login';
 import MenuButton from './menu/menu_button';
 import NavItem from './nav_item';
 import { nav_item_interface } from './nav_item_interface';
@@ -6,7 +7,7 @@ import { nav_item_interface } from './nav_item_interface';
 export default function Nav({ items }: { items: nav_item_interface[] }) {
     return (
         <nav className="
-              dark:bg-blue-900 bg-blue-400 bg-gradient-to-br from-sky-400 via-blue-400 dark:from-indigo-900 dark:via-blue-900 bottom-2 border-b-4 border-green-500 py-3 h-36 flex flex-row
+              hard-gradient bottom-2 border-b-4 border-green-500 py-3 h-36 flex flex-row
         ">
             <ul className="
                 h-4/5 container
@@ -16,6 +17,7 @@ export default function Nav({ items }: { items: nav_item_interface[] }) {
                     <NavItem name={item.name} image={item.image} key={item.name} url={item.url} />
                 ))}
             </ul>
+            <LoginForm />
             {/* <MenuButton /> */}
         </nav>
     )
