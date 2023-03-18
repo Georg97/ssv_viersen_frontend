994508459c09fce5d9b0d3dfff959ad55b397a16
Georg Hirsch <g.hirsch@imrecycling.de>
2023-03-18 18:24:50 +0100

    added edit button to fachschaften nav entries. altough they just show an input field, without any real logic behind ist

--- src/components/fachschaften/FachschaftNavEntry.tsx
+++ src/components/fachschaften/FachschaftNavEntry.tsx
@@ -1,30 +1,53 @@
 import Image from "next/image"
 import { DocumentData } from "firebase/firestore"
+import { useAuthState } from "react-firebase-hooks/auth"
+import { auth } from "@/lib/firebase"
+import { FormEvent, useState } from "react"
 
 export interface FachschaftNavEntryProps {
     entry: DocumentData
     setCurrent: (current: number | null) => void
     currentId: number | null
+    // isEditing: boolean
+    // setIsEditting: (state: boolean) => void
 }
 
 export default function FachschaftNavEntry(props: FachschaftNavEntryProps) {
+    const user = useAuthState(auth)
+    const [editMode, setEditMode] = useState(false);
+    const [currentName, setCurrentName] = useState(props.entry.data().name);
+
+    const currentNameChanged = (event: FormEvent<HTMLInputElement>) => {
+        event.preventDefault()
+        setCurrentName(event.currentTarget.value)
+        // props.setIsEditting(true)
+    }
+
     return (
         <div
-            // className={`flex flex-row items-start mx-auto font-bold text-xl hover:scale-125 transition-all ${props.entry.id == props.currentId ? 'text-blue-500 scale-150 hover:scale-150' : ''}`}
-            // className={`grid grid-cols-6 mx-auto font-bold text-xl hover:scale-125 transition-all ${props.entry.id == props.currentId ? 'text-blue-500 scale-150 hover:scale-150' : ''}`}
-            className={`grid grid-cols-6 mx-auto mt-4 font-bold text-xl hover:cursor-pointer hover:scale-125 origin-left transition-all ${props.entry.id == props.currentId ? 'text-blue-500 scale-150 hover:scale-150' : ''}`}
-            onClick={() => props.setCurrent(props.entry.id)}
+            className="flex flex-row items-center justify-start gap-4"
         >
-            <Image
-                width={240}
-                height={240}
-                src={props.entry.data().icon}
-                alt="Icon"
-                className="h-full block w-auto col-span-1"
-            />
-            <h4 className="col-span-5 ml-4">
-                {props.entry.data().name}
-            </h4>
+            {user[0] && <h4 className="col-span-1 text-3xl hover:cursor-pointer hover:scale-125 transition-all text-green-500" onClick={() => setEditMode(!editMode)}>{editMode ? '✖' : '🖉' }</h4>}
+            <div
+                className={`grid grid-cols-6 mx-auto mt-4 font-bold text-xl hover:cursor-pointer hover:scale-125 origin-left transition-all ${props.entry.id == props.currentId ? 'text-blue-500 scale-150 hover:scale-150' : ''}`}
+                onClick={() => props.setCurrent(props.entry.id)}
+            >
+                <Image
+                    width={240}
+                    height={240}
+                    src={props.entry.data().icon}
+                    alt="Icon"
+                    className="h-full block w-auto col-span-1"
+                />
+                {
+                    editMode ?
+                        <input type="text" value={currentName} onChange={currentNameChanged} className="col-span-4 ml-4 text-stone-900 darktext-stone-300 text-sm"></input>
+                    :
+                    <h4 className="col-span-4 ml-4">
+                        {props.entry.data().name}
+                    </h4>
+                }
+            </div>
         </div>
     )
 }
--- src/components/fachschaften/fachschaften.tsx
+++ src/components/fachschaften/fachschaften.tsx
@@ -132,6 +132,8 @@ import { get } from "https"
 import { DocumentData, QueryDocumentSnapshot } from "firebase/firestore"
 import { Fahkwang } from "next/font/google"
 import FachschaftNavEntry from "./FachschaftNavEntry"
+import { useAuthState } from "react-firebase-hooks/auth"
+import { auth } from "@/lib/firebase"
 
 // export const getServerSideProps: GetServerSideProps = async (ctx) => {
 //   const { data } = await  getFachschaften()
@@ -147,9 +149,11 @@ import FachschaftNavEntry from "./FachschaftNavEntry"
 //   return fachschaften
 // }
 
-// export default function Fachschaften({fachschaften}: {fachschaften: any[]}) {
+// export default function Fachschaften({fachschaften}: {fachschaften: DocumentData[]}) {
 export default function Fachschaften() {
+    const user = useAuthState(auth)
     const [current_id, setCurrent] = useState<number | null>(null)
+    // const [isEditting, setIsEditting] = useState(false);
 
     const [fachschaften, setFachschaften] = useState<DocumentData[] | null>(null);
     useEffect(() => {
@@ -162,17 +166,24 @@ export default function Fachschaften() {
 
     return (
         <div className="flex flex-row">
-            {/* {fachschaftenStrapi && fachschaftenStrapi.data.map(entry => (
-            <p key={entry.id}>{entry.attributes.name}</p>
-          ))} */}
             <div className="flex-initial w-1/4 flex-col gap-4 hidden md:flex">
                 <h3 className="text-4xl font-bold text-center">Fachschaft</h3>
                 {fachschaften && fachschaften.map((entry) => (
-                    // <button key={entry.data().name} className={`mx-auto font-bold text-xl hover:scale-125 transition-all ${entry.id == current_id ? 'text-blue-500 scale-150 hover:scale-150' : ''}`} onClick={() => setCurrent(entry.id)}>
-                    //     {entry.data().name}
-                    // </button>
-                    <FachschaftNavEntry key={entry.id} entry={entry} setCurrent={setCurrent} currentId={current_id} />
+                    <FachschaftNavEntry
+                        key={entry.id}
+                        entry={entry}
+                        setCurrent={setCurrent}
+                        currentId={current_id}
+                    // setIsEditting={setIsEditting}
+                    // isEditing={isEditting}
+                />
                 ))}
+                {user[0] && (
+                    <button className="btn w-fit mx-auto px-2 text-xl font-bold hover:scale-110 mt-8">
+                        <span className="border-r-2 mr-1 pr-1">+</span>
+                        Fachschaft hinzufügen
+                    </button>
+                )}
             </div>
             <div className="flex-initial w-3/4">
                 {/* {current_id && fachschaften.find(v => v.id == current_id) && (
